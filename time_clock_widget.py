d5c7ad5460bf1afda7e1cce2842080c761fd8343
ludovicmantovani <ludovic.mantov@gmail.com>
2023-11-16 22:29:32 +0100

    [UPDATE]: Base structure for time clock widget

--- time_clock_widget.py
+++ time_clock_widget.py
@@ -6,6 +6,29 @@ class TimeClockWidget(QWidget):
         super().__init__()
 
         self.setWindowTitle("TimeClockWidget")
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.create_widgets()
+        self.create_layouts()
+        self.modify_widgets()
+        self.add_widgets_to_layouts()
+        self.setup_connections()
+
+    def create_widgets(self):
+        pass
+
+    def create_layouts(self):
+        pass
+
+    def modify_widgets(self):
+        pass
+
+    def add_widgets_to_layouts(self):
+        pass
+
+    def setup_connections(self):
+        pass
 
 
 if __name__ == '__main__':
